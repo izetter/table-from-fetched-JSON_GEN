466f0c377c9fa2e8313239792cd6dfa9575620b9
izetter <59151193+izetter@users.noreply.github.com>
2023-08-22 23:24:13 -0600

    Fixed missing tbody and tr in thead

--- script.js
+++ script.js
@@ -59,6 +59,8 @@ function createTable(data) {
 	const table = document.createElement('table');
 	table.classList.add('table');
 	const thead = table.createTHead();
+	const theadRow = thead.insertRow();
+	thead.append(theadRow);
 
 	for (let key in data[0]) {
 		const th = document.createElement('th');
@@ -67,15 +69,16 @@ function createTable(data) {
 			.split(' ')
 			.map((word) => word.replace(word[0], word[0].toUpperCase()))
 			.join(' ');
-		thead.append(th);
+		theadRow.append(th);
 	}
 
+	const tbody = table.createTBody();
 	for (let obj of data) {
-		const row = table.insertRow();
+		const row = tbody.insertRow();
 		for (let key in obj) {
 			const cell = row.insertCell();
 			if (/^http.*\.jpg$/.test(obj[key]) || /^http.*\.png$/.test(obj[key])) {
-				cell.setHTML(`<img src=${obj[key]} class="rounded-circle" style="max-width: 3rem" alt="pic" />`);
+				cell.setHTML(`<img src=${obj[key]} class="rounded-circle" style="max-width: 3.5rem" alt="pic" />`);
 			} else {
 				cell.innerText = obj[key];
 			}
